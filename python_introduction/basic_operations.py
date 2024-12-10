e520050b8f7956807169dfde025ed7af4a8e1d7a
NAHOM902 <nahombelayneh387>
2024-12-10 21:58:07 +0300

    modified basic_operations.py file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+addition = number2 + number1
+subtraction = number2 - number1
+multiplication = number2 * number1
+print(f"Addition of 10 and 5 is {addition}")
+print(f"Subtraction of 10 and 5 is {subtraction}")
+print(f"Multiplication of 10 and 5 is {multiplication}")
