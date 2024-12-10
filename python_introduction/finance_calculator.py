3f4665466ffeaf0717bc8ea3379e90ef7301a8eb
NAHOM902 <nahombelayneh387>
2024-12-10 22:56:47 +0300

    added $ sign

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -2,7 +2,7 @@ monthly_income = int(input("Enter your monthly income: "))
 monthly_expense = int(input("Enter your total monthly expense: "))
 
 monthly_saving = monthly_income - monthly_expense
-print(f"Your monthly savings are {monthly_saving}.")
+print(f"Your monthly savings are ${monthly_saving}.")
 
 annual_saving = monthly_saving * 12 + (monthly_saving * 0.05 * 12)
-print(f"Projected savings after one year, with interest, is: {annual_saving}")
+print(f"Projected savings after one year, with interest, is: ${annual_saving}")
