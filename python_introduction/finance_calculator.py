c5f791c34387d08be11eb5ac1903fc4c3b263df2
NAHOM902 <nahombelayneh387@gmail.com>
2024-12-16 09:48:43 +0300

    Update finance_calculator.py
    made 1f instead of 2f

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -5,4 +5,4 @@ monthly_saving = monthly_income - monthly_expense
 print(f"Your monthly savings are ${monthly_saving}.")
 
 annual_saving = monthly_saving * 12 + (monthly_saving * 0.05 * 12)
-print(f"Projected savings after one year, with interest, is: ${annual_saving:.2f}.")
+print(f"Projected savings after one year, with interest, is: ${annual_saving:.1f}.")
