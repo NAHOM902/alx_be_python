f25730a07a5080d8c266484f939df188fd269190
NAHOM <nahombelayneh387@gmail.com>
2024-12-19 18:23:37 -0500

    added

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,17 +1,30 @@
-Task = input("Enter you task: ")
-Time_Bound = input("Is it time-bound (yes/no): ")
-Priority = input("Priority (high/medium/low): ")
+# Task = input("Enter you task: ")
+# Time_Bound = input("Is it time-bound (yes/no): ")
+# Priority = input("Priority (high/medium/low): ")
 
-Reminder = f"'{Task}' is a {Priority} priority task "
-match Priority:
-    case "high":
-        Reminder += "that should be handled urgently "
-    case "medium":
-        Reminder += "that needs attention soon."
-    case "low":
-        Reminder += "that can be done at a convenient time."
-    case _:
-        Reminder = "Invalid priority level. please choose high, medium, or low."
-if Time_Bound == "yes" and Priority in {"high", "medium", "low"}:
-    Reminder += "It requires immediate attention today!"
-print(f"Reminder: {Reminder}")
+# Reminder = f"'{Task}' is a {Priority} priority task "
+# match Priority:
+#     case "high":
+#         Reminder += "that should be handled urgently "
+#     case "medium":
+#         Reminder += "that needs attention soon."
+#     case "low":
+#         Reminder += "that can be done at a convenient time."
+#     case _:
+#         Reminder = "Invalid priority level. please choose high, medium, or low."
+# if Time_Bound == "yes" and Priority in {"high", "medium", "low"}:
+#     Reminder += "It requires immediate attention today!"
+# print(f"Reminder: {Reminder}")
+
+
+
+
+
+
+
+
+
+
+Task = input("Enter your task: ")
+Priority = input("Priority (high/medium/low): ")
+Time_Bound = input("Is it time-bound? (yes/no): ")
