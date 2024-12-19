662f6ff1c5048966b41e626c8b3305951042e96e
NAHOM <nahombelayneh387@gmail.com>
2024-12-19 17:05:15 -0500

    added the file pattern_drawing.py

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,35 @@
+user = int(input("Enter the size fo the pattern: "))
+row = 0
+
+while row < user:
+    for _ in range(user):
+        print("*", end="")
+    print()
+    row += 1
+
+# nahom = 10
+
+# while nahom >= 0:
+#     elsa = 1
+#     while elsa < nahom:
+#         print("*", end="")
+#         elsa += 1
+#     print()
+#     nahom -= 1
+
+
+# name = 10
+# row = 1
+# while name >= row:
+#     space = name - row
+#     j = 1
+#     while j <= space:
+#         print(" ", end="")
+#         j += 1
+#     k = 1
+#     while k <= (2 * row - 1):
+#         print("*", end="")
+#         k += 1
+#     print()
+#     row += 1
+    
