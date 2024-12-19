cd748a9aa6e05bb6f36c20b54474c5d19bbdc390
NAHOM <nahombelayneh387@gmail.com>
2024-12-19 18:03:16 -0500

    added

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,17 +1,17 @@
 Task = input("Enter you task: ")
-Priority = input("Priority (high/medium/low): ").lower()
 Time_Bound = input("Is it time-bound (yes/no): ").lower()
+Priority = input("Priority (high/medium/low): ").lower()
 
-Reminder = f"'{Task}' is a {Priority} priority task"
+Reminder = f"'{Task}' is a {Priority} priority task "
 match Priority:
     case "high":
-        Reminder += "that should be handled urgently"
+        Reminder += "that should be handled urgently "
     case "medium":
         Reminder += "that needs attention soon."
     case "low":
         Reminder += "that can be done at a convenient time."
     case _:
         Reminder = "Invalid priority level. please choose high, medium, or low."
-if Time_bound == "yes" and Priority in {"high", "medium", "low"}:
+if Time_Bound == "yes" and Priority in {"high", "medium", "low"}:
     Reminder += "It requires immediate attention today!"
 print(f"Reminder: {Reminder}")
